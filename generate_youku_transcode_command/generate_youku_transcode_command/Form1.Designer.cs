d336040daf604469abc6c5444b514d6ff65ed6b8
unknown <kora.hacker@qq.com>
2015-06-21 18:41:36 +0800

    first commit

---
+++ generate_youku_transcode_command/generate_youku_transcode_command/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace generate_youku_transcode_command
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ui_textbox_src = new System.Windows.Forms.TextBox();
+            this.ui_button_select = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ui_textbox_des = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ui_msg_out = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "src";
+            // 
+            // ui_textbox_src
+            // 
+            this.ui_textbox_src.Location = new System.Drawing.Point(41, 6);
+            this.ui_textbox_src.Name = "ui_textbox_src";
+            this.ui_textbox_src.Size = new System.Drawing.Size(692, 21);
+            this.ui_textbox_src.TabIndex = 1;
+            // 
+            // ui_button_select
+            // 
+            this.ui_button_select.Location = new System.Drawing.Point(739, 4);
+            this.ui_button_select.Name = "ui_button_select";
+            this.ui_button_select.Size = new System.Drawing.Size(75, 23);
+            this.ui_button_select.TabIndex = 2;
+            this.ui_button_select.Text = "选择";
+            this.ui_button_select.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "des";
+            // 
+            // ui_textbox_des
+            // 
+            this.ui_textbox_des.Location = new System.Drawing.Point(41, 38);
+            this.ui_textbox_des.Name = "ui_textbox_des";
+            this.ui_textbox_des.Size = new System.Drawing.Size(692, 21);
+            this.ui_textbox_des.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(739, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ui_msg_out
+            // 
+            this.ui_msg_out.Location = new System.Drawing.Point(14, 75);
+            this.ui_msg_out.Multiline = true;
+            this.ui_msg_out.Name = "ui_msg_out";
+            this.ui_msg_out.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ui_msg_out.Size = new System.Drawing.Size(800, 451);
+            this.ui_msg_out.TabIndex = 6;
+            this.ui_msg_out.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 538);
+            this.Controls.Add(this.ui_msg_out);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ui_textbox_des);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ui_button_select);
+            this.Controls.Add(this.ui_textbox_src);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ui_textbox_src;
+        private System.Windows.Forms.Button ui_button_select;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ui_textbox_des;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox ui_msg_out;
+    }
+}
+
---
+++ generate_youku_transcode_command/generate_youku_transcode_command/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace generate_youku_transcode_command
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string simple_uuid_string()
+        {
+            string s = System.Guid.NewGuid().ToString();
+            s = s.Substring(0, 8);
+            return s;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* 设置一些默认值 */
+            ui_textbox_src.Text = @"D:\Youku Files\download";
+            ui_textbox_des.Text = @"C:\Documents and Settings\kora.hacker\桌面\罗辑思维";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 读取src中的文件夹，然后开始生成转换命令
+            string[] src_file_path = Directory.GetFiles(ui_textbox_src.Text, "*.kux");
+            
+            /*ui_msg_out.AppendText(Convert.ToString(src_file_path.Length) + "\n");*/
+
+            /*for (int i = 0; i < src_file_path.Length; i++)
+            {
+                ui_msg_out.AppendText(src_file_path[i] + "\n");
+            }
+
+            for (int i = 0; i < src_file_path.Length; i++)
+            {
+                string file_name = Path.GetFileNameWithoutExtension(src_file_path[i]);
+                ui_msg_out.AppendText(file_name + "\n");
+            }*/
+
+            StreamWriter sw = new StreamWriter(@"C:\Documents and Settings\kora.hacker\桌面\" + simple_uuid_string() + ".bat");
+            
+            ui_msg_out.AppendText("@echo off\r\n");
+            ui_msg_out.AppendText("cls\r\n");
+
+            sw.WriteLine("@echo off");
+            sw.WriteLine("cls");
+
+
+            for (int i = 0; i < src_file_path.Length; i++)
+            {
+                string command_string = "ffmpeg -y -i \"" + src_file_path[i] + "\"  -vcodec copy -acodec copy -f mp4 \"" + ui_textbox_des.Text + "\\" + Path.GetFileNameWithoutExtension(src_file_path[i]) + ".mp4\"";
+                ui_msg_out.AppendText(command_string + "\r\n");
+                sw.WriteLine(command_string);
+                string move_string = "move \"" + src_file_path[i] + "\" \"" + ui_textbox_src.Text + "\\done\"";
+                ui_msg_out.AppendText(move_string + "\r\n");
+                sw.WriteLine(move_string);
+            }
+            
+            ui_msg_out.AppendText("pause\r\n");
+            sw.WriteLine("pause");
+
+            sw.Close();
+        }
+
+        
+    }
+}
